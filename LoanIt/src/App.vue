b3d9cc67f874686da81fdb90fdc652af910025af
Finn Gorell <finnsg2k@gmail.com>
2025-11-10 22:58:06 +0100

    Added input will now be displayed in an unordered list - Placeholder if no items are listed

--- LoanIt/src/App.vue
+++ LoanIt/src/App.vue
@@ -10,5 +10,7 @@ import HomeView from './views/HomeView.vue'
 </script>
 
 <style>
-
+h1, h2, h3 { margin-bottom: 0.5rem; }
+section { margin: 1rem; }
+div { margin-bottom: 1rem; }
 </style>
--- LoanIt/src/views/HomeView.vue
+++ LoanIt/src/views/HomeView.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { ref } from 'vue'
+const items = ref([])
 
+const newItem = ref('')
+
+function addItem() {
+  const name = newItem.value.trim()
+  if (!name) return
+  items.value.push({name})
+  newItem.value = ''
+}
 </script>
 
 <template>
@@ -9,5 +19,13 @@
             <input v-model="newItem" placeholder="Neuen Gegenstand eingeben" />
             <button @click="addItem">Hinzufügen</button>
         </div>
+        <div v-if="items.length > 0">
+            <ul>
+                <li v-for="item in items" :key="item.id">
+                    {{ item.name }}
+                </li>
+            </ul>
+        </div>
+    <p v-else>Du hast noch keine Gegenstände hinzugefügt.</p>
   </section>
 </template>
