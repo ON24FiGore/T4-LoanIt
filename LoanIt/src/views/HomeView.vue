877fd4c7ec6162d3ee552ecbeaccdf5fcc8e2c4b
Finn Gorell <finnsg2k@gmail.com>
2025-11-10 23:20:18 +0100

    Items can be deleted - Temporary solution with id : Date.now() - Preparation for loaned item list

--- LoanIt/src/views/HomeView.vue
+++ LoanIt/src/views/HomeView.vue
@@ -1,14 +1,18 @@
 <script setup>
 import { ref } from 'vue'
 const items = ref([])
-
 const newItem = ref('')
+const borrowedItems = ref([])
 
 function addItem() {
   const name = newItem.value.trim()
   if (!name) return
-  items.value.push({name})
+  items.value.push({id: Date.now(),name})
   newItem.value = ''
+}
+
+function removeItem(id) {
+  items.value = items.value.filter(item => item.id !== id)
 }
 </script>
 
@@ -23,9 +27,24 @@ function addItem() {
             <ul>
                 <li v-for="item in items" :key="item.id">
                     {{ item.name }}
+                    <button @click="removeItem(item.id)">Löschen</button>
                 </li>
             </ul>
         </div>
     <p v-else>Du hast noch keine Gegenstände hinzugefügt.</p>
   </section>
+
+  <section>
+    <h2>Ausgeliehene Gegenstände</h2>
+
+    <div v-if="borrowedItems.length > 0">
+      <ul>
+        <li v-for="item in borrowedItems" :key="item.id">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+    <p v-else>Du hast aktuell keine Gegenstände ausgeliehen.</p>
+  </section>
 </template>
+
